0af405158d7e990190b54459631d0b2899399ab1
lektphire <93264392+lektphire@users.noreply.github.com>
2024-07-07 20:20:57 -0500

    Removed unnecessary React import

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import {useState} from 'react'
 import {Menu} from "antd";
 import {HomeOutlined, SettingOutlined, NodeIndexOutlined} from '@ant-design/icons';
 import "../css/MenuList.css";
